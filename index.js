dcb8e76475e74bb9dd2a27945e543a28b3034bfc
Mike Boehm <mike@boehm.com.au>
2016-04-24 12:59:18 +0100

    Console log 'toggle'

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-
+    console.log('Toggle!');
     applescript.execString(script, function(err) {});
     res.send('');
 });
