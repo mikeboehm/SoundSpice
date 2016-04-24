3084b543b3b0c07bd935a51cfb2a16a6d9baa43a
Mike Boehm <mike@boehm.com.au>
2016-04-24 12:58:49 +0100

    Fixed indenting

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  applescript.execString(script, function(err) {});
-  res.send('');
+
+    applescript.execString(script, function(err) {});
+    res.send('');
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
