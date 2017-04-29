a9dce3aab1b7b0aa28d618ecae6a960334065736
Mike Boehm <mike@boehm.com.au>
2017-04-29 17:01:04 +0100

    button styling

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var app = express();
 app.get('/', function (req, res) {
     var timestamp = Math.round(new Date().getTime()/1000).toString();
 
-    res.send('<a href="/toggle/?timestamp=' + timestamp + '">Play/pause again</a><pre>/toggle</pre>');
+    res.send('<a href="/toggle/?timestamp=' + timestamp + '" style="color: #000000; text-decoration: none; font-size: 2em; line-height: 1.4em; font-family: Helvetica, Arial, san-serif;"><div style="background-color: #EEEEEE;text-align: center; box-shadow: 0px 5px 10px 2px rgba(0,0,0,0.12); border-radius: 5px; max-width: 750px; border: 1px solid #AAAAAA; margin: 0 auto; margin-top: 50px; height: 50px;">Play/pause again</div></a>');
 });
 
 // Using a timestamp to make sure the request is fresh
